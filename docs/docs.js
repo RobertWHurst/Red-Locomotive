c9d756b98d094a0d33b6bec5a3e03e3ef47fcb2a
Robert Hurst <robert@thinktankdesign.ca>
2011-09-23 03:40:04 -0700

    Summary: Rewrote the docs.js. Hash tag paths are now implemented. * Improved docs.js. It now updates on change of the hash tag.

--- docs/docs.js
+++ docs/docs.js
@@ -1,190 +1,349 @@
 jQuery(function(){
 
 	var modules = jQuery('#modules'),
-		content = jQuery('#content');
+		content = jQuery('#content'),
 
-	//align sidebar and content
-	setInterval(function(){
+		pages = {};
 
-		var sidebar = jQuery('#sidebar').css('height', 'auto'),
-			content = jQuery('#content').css('height', 'auto');
+	(function MaintainColumnHeight(){
 
-		if(sidebar.height() > content.height()) {
-			content.height(sidebar.height());
-		} else {
-			sidebar.height(content.height());
-		}
-	}, 100);
+		//align sidebar and content
+		setInterval(function(){
 
-	//renders the content pane
-	function buildContent(data, breadCrumb) {
+			var sidebar = jQuery('#sidebar').css('height', 'auto'),
+				content = jQuery('#content').css('height', 'auto');
 
-		//clear the content
-		content.html('');
+			if(sidebar.height() > content.height()) {
+				content.height(sidebar.height());
+			} else {
+				sidebar.height(content.height());
+			}
+		}, 100);
 
-		var args = '';
+	})();
 
-		if(data.arguments) {
-			for(var i = 0; i < data.arguments.length; i += 1) {
-				args += (typeof data.arguments[i]["default"] !== 'undefined' ? '<span class="optional">[' + data.arguments[i].name + ']</span>' : data.arguments[i].name);
-				if(i < data.arguments.length - 1) {
-					args += ', ';
-				}
+	function LoadCurrentHash() {
+		if(location.hash) {
+			var page = location.hash.substr(1);
+			if(pages[page]) {
+				console.log(page);
+				content.html('').append(pages[page]);
 			}
 		}
+	}
 
-		//create a content title
-		jQuery('<h1>' + (data.isModule && 'Module - ' || '') + data.name + (!data.methods && !data.objects && '()' || !data.isModule && '{}' || '') + '</h1>').appendTo(content);
+	//renders the content pane
+	function buildContent(data, path) {
 
-		//set the description
-		if(data.description) {
-			jQuery('<h3>Description:</h3><p>' + data.description + '</p>').appendTo(content);
+		function pageTitle() {
+			
+			//the suffix
+			var suffix = (data.arguments && '()' || ''),
+
+			//the prefix
+				prefix = (data.moduleName && 'Module - ' || '');
+
+			//the title
+			return jQuery('<h1>' + prefix + (data.name || data.moduleName) + suffix + '</h1>');
+
 		}
 
-		//add the bread crumb
-		if(!data.isModule) {
-			jQuery('<h3>Usage:</h3>').appendTo(content);
-			jQuery('<h4>' + (!data.isGlobal && breadCrumb + '.' || '') + data.name + (data.methods || data.objects ? '' : '(<span class="arguments">' + args + '</span>)') + '</h4>').appendTo(content);
-		}
+		//builds an arguments table for methods
+		function argumentsTable() {
 
-		//build the arguments table
-		if(data.arguments) {
-			(function(){
+			//make sure there are arguments
+			if(data.arguments) {
 
-				jQuery('<h3>Arguments:</h3>').appendTo(content);
-				var table = jQuery('<table><thead><tr><tr><th>Position</th><th>Name</th><th>Accepts</th><th>Default</th><th>Description</th></tr></thead></table>').appendTo(content);
+				//create a wrapping div
+				var container = jQuery('<div id="argumentsTable"></div>'),
 
+				//create a heading
+					heading = jQuery('<h3>Arguments:</h3>'),
+				
+				//create the table elements
+					tableElement = jQuery('<table></table>'),
+					tableHead = jQuery('<thead><tr><th></th><th>Name</th><th>Accepts</th><th>Default</th><th>Description</th></tr></thead>'),
+					tableBody = jQuery('<tbody></tbody>');
+
+				//create each argument row
 				for(var i = 0; i < data.arguments.length; i += 1) {
 
-					var type = '',
-						name = data.arguments[i].name,
-						argDefault = (typeof data.arguments[i]["default"] !== 'undefined' ? data.arguments[i]["default"] : 'none'),
-						description = (typeof data.arguments[i]["default"] !== 'undefined' ? '(optional) ' : '') + (data.arguments[i].description || '');
+					//grab the argument
+					var argumentData = data.arguments[i],
 
-					//get the argument types
-					if(typeof data.arguments[i].type === 'object') {
-						for(var ii = 0; ii < data.arguments[i].type.length; ii += 1) {
-							type += data.arguments[i].type[ii];
-							if(ii < data.arguments[i].type.length - 1) {
-								type += ', ';
+					//collect some info about the arguments and build table cells
+						row = jQuery('<tr id="arg-' + i + '" class="argument"></tr>'),
+						position = jQuery('<td class="position">' + i + '</td>'),
+						name = jQuery('<td class="argument name">' + argumentData.name + '</td>'),
+						description = jQuery('<td class="argument description">' + (argumentData.description || '') + '</td>'),
+						defaultValue = jQuery('<td class="argument default">' + (argumentData["default"] || '') + '</td>'),
+						allowedTypes = '';
+
+					//generate the allowed 'type' cell
+					if(argumentData.type) {
+						if(typeof argumentData.type === "object") {
+							for(var ii = 0; ii < argumentData.type.length; ii += 1) {
+								allowedTypes += argumentData.type[ii];
+								if(ii < argumentData.type.length - 1) {
+									allowedTypes += ', ';
+								}
 							}
+						} else {
+							allowedTypes = argumentData.type;
 						}
 					} else {
-						type = (data.arguments[i].type || '?');
+						allowedTypes = '?';
 					}
+					allowedTypes = jQuery('<td class="type">' + allowedTypes + '</td>');
 
-					jQuery('<tr><td class="position">' + i + '</td><td class="name">' + name + '</td><td class="type">' + type + '</td><td class="default">' + argDefault + '</td><td class="description">' + description + '</td></tr>').appendTo(table);
+					//add the row to the table
+					tableBody.append(row.append(position, name, allowedTypes, defaultValue, description));
+				}
 
-					args += data.arguments[i].name;
-					if(i < data.arguments.length - 1) {
-						args += ', ';
+				//assemble and return the new table
+				return container.append(heading, tableElement.append(tableHead, tableBody));
+			}
+
+			return false;
+		}
+
+		//builds an arguments table for methods
+		function returnsTable() {
+
+			//make sure there are arguments
+			if(data.returns) {
+
+				//create a wrapping div
+				var container = jQuery('<div id="returnsTable"></div>'),
+
+				//create a heading
+					heading = jQuery('<h3>Returns:</h3>'),
+				
+				//create the table elements
+					tableElement = jQuery('<table></table>'),
+					tableHead = jQuery('<thead><tr><th>Name</th><th>Type</th><th>Description</th></tr></thead>'),
+					tableBody = jQuery('<tbody></tbody>'),
+
+					//collect some info about the arguments and build table cells
+					row = jQuery('<tr id="returns" class="return"></tr>'),
+					name = jQuery('<td class="return name">' + data.returns.name + '</td>'),
+					desc = jQuery('<td class="return description">' + (data.returns.description || '') + '</td>'),
+					type = jQuery('<td class="type">' + data.returns.type + '</td>');
+
+				//assemble and return the new table
+				return container.append(heading, tableElement.append(tableHead, tableBody.append(row.append(name, type, desc))));
+			}
+
+			return false;
+		}
+
+		function methodUsage() {
+
+			if(!data.methods && !data.objects) {
+
+				//create a wrapping div
+				var container = jQuery('<div id="usageExample"></div>'),
+
+				//create a heading
+					heading = jQuery('<h3>Usage:</h3>'),
+
+					usagePath = '.' + path.split('-')[1];
+
+				//create the usage example
+					usage = (!data.isGlobal && usagePath || data.name) + '(';
+
+				//make sure there are arguments
+				if(data.arguments) {
+					for(var i = 0; i < data.arguments.length; i += 1) {
+						var argumentData = data.arguments[i];
+
+						//mash the argument name on to the end of the usage string
+						usage += (typeof argumentData["default"] !== 'undefined' && '<span class="argument optional">[' + argumentData.name + ']</span>' || '<span class="argument">' + argumentData.name + '</span>');
+
+						if(i < data.arguments.length - 1) {
+							usage += ', '
+						}
 					}
 				}
-			})();
+
+				//wrap the usage example in a heading 4 tag
+				usage = jQuery('<h4>' + usage + ')</h4>');
+
+				//assemble and return
+				return container.append(heading, usage);
+
+			}
+
+			return false;
 		}
 
-		//build the return table
-		if(data.returns) {
+		function parseDescription() {
+			
+			//create a wrapper
+			var container = jQuery('<div id="description"></div>'),
 
-			jQuery('<h3>Returns:</h3>').appendTo(content);
-			(function(){
-				var name = (data.returns.name || '?'),
-					type = (data.returns.type || '?'),
-					description = (data.returns.description || '');
+			//TODO: Parse with markdown
+			//	description = jQuery('<p>' + markdown(data.description) + '</p>');
+				description = jQuery('<p>' + data.description + '</p>');
 
-				jQuery('<table><thead><tr><tr><th>Name</th><th>Type</th><th>Description</th></tr></thead><tr><td class="name">' + name + '</td><td class="type">' + type + '</td><td class="description">' + description + '</td></tr></table>').appendTo(content);
-
-			})();
+			return container.append(description);
 		}
 
-		//output the doc source
-		jQuery('<h3 class="source">Doc Source:</h3><code id="JSONDATA">' + JSON.stringify(data) + '</code>').appendTo(content);
+		function pageJSONBlock() {
+			return jQuery('<div class="JSON"><h3 class="source">Doc Source:</h3><code id="JSONDATA">' + JSON.stringify(data) + '</code></div>');
+		}
+
+		var page = jQuery('<div></div>'),
+			title = pageTitle(),
+			description = parseDescription(),
+			usage = methodUsage(),
+			args = argumentsTable(),
+			returns = returnsTable(),
+			json = pageJSONBlock();
+
+			page.append(title, description);
+
+			if(usage) {
+				page.append(usage);
+			}
+
+			if(args) {
+				page.append(args);
+			}
+
+			if(returns) {
+				page.append(returns);
+			}
+
+			page.append(json);
+
+			//return the new page
+			return page.children();
 
 	}
 
+	function createSection(heading) {
+
+		//set the heading
+		var heading = jQuery('<li>' + heading + '</li>'),
+			listElement = jQuery('<ul></ul>');
+
+		//add the heading to the list element
+		heading.append(listElement);
+
+		//return the section
+		return heading;
+	}
+
 	//renders a link tree for the sidebar
-	function handleObject(data, target, heading, breadCrumb) {
+	function createObjectLinks(data, parent, parentPath) {
 
-		breadCrumb = breadCrumb || '';
+		//if the tree has not started create it
+		parentPath = parentPath || '';
 
-		//add the heading
-		if(heading) {
-			jQuery('<li>' + heading + '</li>').appendTo(target);
-			target = jQuery('<ul></ul>').appendTo(target);
+		//create a heading for the object
+		if(parentPath) {
+
+			//create a section
+			var list = createSection('Objects:');
+			parent.append(list);
+
+			//shift the parenthood to the new section
+			parent = list.find('ul:first');
+
 		}
 
-		//loop through each module and create a link for it
+		//loop through each object and create a link for it
 		for(var i = 0; i < data.length; i += 1) {
 
-			//grab the module data
-			var object = data[i],
+			//grab the object data
+			var objectData = data[i],
+
+			//add this object to the tree
+				path = parentPath + (objectData.name && objectData.name + '.' || objectData.moduleName + '-'),
 
 			//create a list item and link for the current object
-			listItem = createLink(object, target, breadCrumb);
+				listItem = createLink(objectData, path);
 
-			//extend the bread crumb
-			var objBreadCrumb = '';
-			if(object.name && !object.isModule) {
-				objBreadCrumb = breadCrumb + '.' + object.name;
-			} else {
-				objBreadCrumb = breadCrumb;
-			}
+			//build the page content
+			pages[path] = buildContent(objectData, path);
 
-			if(object.methods || object.objects) {
+			//append the list item to the document
+			parent.append(listItem);
+
+			if(objectData.methods || objectData.objects) {
 
 				//create a list
 				var list = jQuery('<ul></ul>').appendTo(listItem);
 
-				if(object.methods) {
+				if(objectData.methods) {
 					//the method links
-					createMethodLinks(object.methods, list, 'Methods:', objBreadCrumb);
+					createMethodLinks(objectData.methods, list, path);
 				}
 
-				if(object.objects) {
+				if(objectData.objects) {
 					//handle any sub elements
-					handleObject(object.objects, list, 'Objects:', objBreadCrumb);
+					createObjectLinks(objectData.objects, list, path);
 				}
 			}
 		}
 	}
 
 	//creates links for an array of methods
-	function createMethodLinks(methods, target, heading, breadCrumb) {
+	function createMethodLinks(data, parent, parentPath) {
 
-		//add the heading
-		if(heading) {
-			jQuery('<li>' + heading + '</li>').appendTo(target);
-			target = jQuery('<ul></ul>').appendTo(target);
-		}
+		//create a section
+		var list = createSection('Methods:');
+		parent.append(list);
 
-		for(var i = 0; i < methods.length; i += 1) {
-			var method = methods[i];
+		//shift the parenthood to the new section
+		parent = list.find('ul:first');
+
+		for(var i = 0; i < data.length; i += 1) {
+			var methodData = data[i],
+
+				//update the path
+				path = parentPath + methodData.name,
 
 			//create a link for the method
-			createLink(method, target, breadCrumb);
+				listItem = createLink(methodData, path);
+
+			//build the page content
+			pages[path] = buildContent(methodData, path);
+
+			//attach the new list item
+			parent.append(listItem);
+
 		}
 	}
 
 	//creates a link method the executes a callback when clicked
-	function createLink(object, target, breadcrumb) {
-		var link = jQuery('<li><a href="#' + object.name.toLowerCase().replace(/\s/g, '') + '" title="' + (object.description || object.name) + '">' + object.name + '</a></li>'),
-			a = link.find('a');
+	function createLink(data, path) {
 
-		a.click(function(event) {
-			event.stopPropagation();
-			buildContent(object, breadcrumb);
-		});
-		
-		link.appendTo(target);
+		//set some variables
+		var hash = '#' + path,
+			title = data.description || data.name,
+			name = data.name || data.moduleName,
 
-		return link;
+		//create the new dom nodes
+			listItemElement = jQuery('<li></li>'),
+			anchorElement = jQuery('<a href="' + hash + '" title="' + title + '">' + name + '</a>');
+
+		//pair the list item and the anchor
+		listItemElement.append(anchorElement);
+
+		//return the list item
+		return listItemElement;
 	}
 
+	//get the json data
 	jQuery.ajax({
 		"url": "modules.json",
 		"dataType": "json",
 		"success": function (data) {
-			handleObject(data, modules);
+			createObjectLinks(data, modules);
+			LoadCurrentHash();
+			jQuery(window).bind('hashchange', LoadCurrentHash);
 		}
 	});
 
