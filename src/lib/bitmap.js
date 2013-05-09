2717ce468ff0bd9f436529eb4a1797c6e94acb09
Robert Hurst <robertwhurst@gmail.com>
2013-05-09 16:19:32 -0700

    Hack to prevent browserify from trying to bundle node-canvas
    See https://github.com/substack/node-browserify/pull/378

--- src/lib/bitmap.js
+++ src/lib/bitmap.js
@@ -43,7 +43,7 @@ function Canvas(width, height) {
         canvas.height = height;
     }
     else {
-        var canvas = new (require('canvas'))(width, height);
+        var canvas = new (require('canvas' + ''))(width, height);
     }
 
     return canvas;
