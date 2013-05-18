e465e93a2bcc3836059866ae83d8c8ddd8dcf4dd
Robert Hurst <robertwhurst@gmail.com>
2013-05-18 00:19:07 -0700

    Allow the core clock to be set in options.

--- src/modules/core.js
+++ src/modules/core.js
@@ -6,7 +6,7 @@ module.exports = Core;
 
 function Core(engine, options) {
     var logs = {};
-    var clock = Clock(1000);
+    var clock = Clock(options.coreClock || 1000);
 
     Emitter(engine);
     engine.start = clock.start;
